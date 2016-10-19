1a358463382d54f478be5c10bb8bcd5e966f5045
tomeksamcik <tomek@samcik.pro>
2016-10-19 21:20:06 +0200

    Comments

--- src/main/java/com/nespresso/sofa/recruitement/tournament/Armor.java
+++ src/main/java/com/nespresso/sofa/recruitement/tournament/Armor.java
@@ -6,10 +6,24 @@ public class Armor {
 
 	private final Integer reduceDamageGivenBy = 1;
 
+	/**
+	 * Reduces damge received as a result of a hit
+	 * 
+	 * @param damage
+	 *            Damage before armor protection
+	 * @return Damage after armor protection
+	 */
 	public Integer reduceDamageReceived(Integer damage) {
 		return damage - reduceDamageReceivedBy;
 	}
 
+	/**
+	 * Decreases damage incurred by warrior due to increased mass
+	 * 
+	 * @param damage
+	 *            Damage before armor penalty
+	 * @return Damage after applying armor penalty
+	 */
 	public Integer reduceDamageGiven(Integer damage) {
 		return damage - reduceDamageGivenBy;
 	}
